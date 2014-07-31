85b78a833c35e565b86f177ad2e73ff92608cba6
yag12 <yag12@lycos.co.kr>
2014-07-31 15:59:36 +0900

    DB connected

--- library/Model/Mysqli.php
+++ library/Model/Mysqli.php
@@ -7,5 +7,26 @@ namespace Library\Model;
 
 class Mysqli
 {
+	protected $db;
 
+	/**
+	* @Desc construct
+	* @Param Info.db $db
+	* @Return void
+	*/
+	public function __construct(&$db)
+	{
+		$this->db = &$db;
+		$this->connection();
+	}
+
+	/**
+	* @Desc mongodb connect
+	* @Param void
+	* @Return void
+	*/
+	protected function connection()
+	{
+
+	}
 }
